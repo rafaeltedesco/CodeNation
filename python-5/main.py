4f10a326f5832c073593f15294e9dce383b301db
Rafael Tedesco <tedesco.rafaelmoutinho@gmail.com>
2020-05-13 18:51:06 -0300

    updated some comments in main

--- python-5/main.py
+++ python-5/main.py
@@ -1,31 +1,6 @@
 """
 Author: Rafael Tedesco - rocket.py
 Date: 13/05/20
-
-Instructions and goals:
-
-Tarifação:
-Diurna - entre 6h00 e 22h00 :
-    * Encargo permanente: R $ 0,36 (encargos fixos que são usados ​​para pagar o custo da conexão);
-    * Taxa de ligação / minuto: R $ 0,09 (não há cobrança fracionada. *A cobrança se aplica a cada ciclo concluído de 60 segundos)*.
-Noturna - entre 22h00 e 6h00:
-    * Taxa permanente: R $ 0,36
-    * Taxa de ligação / minuto: R $ 0,00
-
-Classificar as ligações por número de origem e agrupá-las com o valor total das ligações feitas por esse número. #(done)#
-Complete o método classify_by_phone_number que recebe por parâmetro um relatório em forma de lista de dicionários.
-Os horários de início de fim das ligações estão no formato timestamp e os números de telefone no formato de strings, conforme a estrutura abaixo.
-
-Com o algoritmo apropriado, retorne uma lista contendo um dicionário com duas chaves source e total para cada número de origem, ordenado pelo maior valor. O valor total deve ser arredondado em 2 casas decimais.
-
-Observações:
-O calculo deve ser feito considerando o horário de cada minuto, por exemplo, uma mesma ligação pode ter tarifas diferentes se iniciar no período diurno e for finalizada no período noturno, ou seja, cada minuto deve ser tarifado conforme o seu horário inicial.
-Para esse desafio considera apenas ligações que iniciam e terminam no mesmo dia
-Exemplo de retorno
-[
-    {'source': '48-996355555', 'total': 234.89},
-    {'source': '41-885633788', 'total': 124.89},
-]
 """
 
 from datetime import datetime
@@ -72,10 +47,6 @@ def get_costs(records):
     return records
     
 def classify_by_phone_number(records):
-    """
-    Com o algoritmo apropriado, retorne uma lista contendo um dicionário com duas chaves source e 
-    total para cada número de origem, ordenado pelo maior valor. O valor total deve ser arredondado em 2 casas decimais.
-    """
     total_bills = []
     records_cost = get_costs(records)
     
